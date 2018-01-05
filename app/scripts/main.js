9e89c0dd8ed37b41a555699bdbef349ae192f750
Frederikam <frogkr@gmail.com>
2018-01-05 20:06:07 +0100

    Preloading

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
 import mainMenu from './ui/mainMenu.js'
+import preload from './preload.js'
 const module = {};
 
 module.stage = new createjs.Stage("game");
@@ -7,14 +8,21 @@ module.stage = new createjs.Stage("game");
 module.background = new createjs.Container();
 module.foreground = new createjs.Container();
 module.ui         = new createjs.Container();
+module.dialogue   = new createjs.Container();
 
 module.stage.addChild(module.background);
 module.stage.addChild(module.foreground);
 module.stage.addChild(module.ui);
+module.stage.addChild(module.dialogue);
+
 module.stage.enableMouseOver(20);
 createjs.Ticker.setFPS(60);
 createjs.Ticker.addEventListener("tick", module.stage);
 
+preload.load(function() {
+  mainMenu.run(module.stage);
+});
+
 export default module
-mainMenu.run(module.stage);
+
 
