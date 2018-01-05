f105216648dcde199a5e0854b0ffa2841ccfe28e
Frederikam <frogkr@gmail.com>
2018-01-05 20:36:45 +0100

    Loading spinner

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,8 +19,26 @@ module.stage.enableMouseOver(20);
 createjs.Ticker.setFPS(60);
 createjs.Ticker.addEventListener("tick", module.stage);
 
+const spinnerContainer = new createjs.Container();
+spinnerContainer.x = 1920/2;
+spinnerContainer.y = 1080/2;
+const spinnerImg = new createjs.Bitmap("assets/images/loading_spinner.png");
+spinnerImg.x = -64;
+spinnerImg.y = -64;
+spinnerImg.width = 128;
+spinnerImg.height = 128;
+module.stage.addChild(spinnerContainer);
+spinnerContainer.addChild(spinnerImg);
+createjs.Tween.get(spinnerContainer, {loop:true})
+  .to({rotation: 360}, 1000);
+
+
 preload.load(function() {
-  mainMenu.run(module.stage);
+  createjs.Tween.get(spinnerContainer)
+    .to({alpha: 0}, 500)
+    .call(function() {
+      mainMenu.run(module.stage);
+    });
 });
 
 export default module
