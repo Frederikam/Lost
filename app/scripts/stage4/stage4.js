47ee443ab23f9ce08a861d8a8c063d3cfd1bb502
chaoulgs <chaoulgs@vcu.edu>
2018-01-07 00:58:21 -0500

    map locks and unlockers to respective points

--- app/scripts/stage4/stage4.js
+++ app/scripts/stage4/stage4.js
@@ -3,7 +3,6 @@ import main from "../main.js"
 const module = {};
 
 const imgWidth = 700;
-module.background = "assets/images/stage3/background3.jpg";
 
 let gridContainer;
 
@@ -19,26 +18,29 @@ module.run = function () {
     	.wait(2200)
     	.to({alpha: 1}, 2000);
 
-	const rows = 8;
+	const rows = 10;
 	const columns = 9;
 
 	let grid = {};
 	let points = [];
 
-	for (let x = 0; x < rows; x++) {
-		for (let y = 0; y < columns; y++) {
+	for (let x = 0; x < columns; x++) {
+		let pointRow = [];
+		for (let y = 0; y < rows; y++) {			
 			let point = new createjs.Shape();
-
 			point.graphics.beginFill("#FFF").drawCircle(0,0,3);
 			point.set({x: x * imgWidth/rows, y: y * imgWidth/columns});
-
 			gridContainer.addChild(point);
-			points.push(point);
+			pointRow.push(point);			
 		}
-	}	
+		points.push(pointRow);
+	}
+
+	console.log(points);
 	main.stage.update();
 
 	let ball = {x: 0, y: 5};
+
 	let sliders = [
 		//x,y to x,y
 		//0,1 to 1,1
@@ -58,19 +60,37 @@ module.run = function () {
 	];
 
 	let locks = [
-		{x: 8, y: 0, color: "yellow"},
+		{x: 6, y: 1, color: "yellow"},
 		{x: 6, y: 7, color: "red"}
 	];
+
+	locks.forEach(function(lock) {
+		let lockPoint = points[lock.x][lock.y];
+		let lockIcon = new createjs.Shape();
+		lockIcon.graphics.beginFill(lock.color).drawCircle(0,0,8);
+		lockIcon.set({x: lockPoint.x, y: lockPoint.y});
+		gridContainer.addChild(lockIcon);
+	});
 
 	let unlocks = [
 		{x: 8, y: 0, color: "yellow"},
 		{x: 8, y: 9, color: "red"},
 	];
+
+	unlocks.forEach(function(unlock) {
+		let unlockPoint = points[unlock.x][unlock.y];
+		let unlockIcon = new createjs.Shape();
+		unlockIcon.graphics.setStrokeStyle(2).beginStroke(unlock.color).drawCircle(0,0,8);
+		unlockIcon.set({x: unlockPoint.x, y: unlockPoint.y});
+		gridContainer.addChild(unlockIcon);
+	});
+
+	main.stage.update();
 };
 
 function tick() {
   // Get mouse position relative to container
-  const pt = mapContainer.globalToLocal(mapContainer.stage.mouseX, mapContainer.stage.mouseY);
+  const pt = gridContainer.globalToLocal(gridContainer.stage.mouseX, gridContainer.stage.mouseY);
   mouseDownEvent.currentTarget.x = pt.x;
   mouseDownEvent.currentTarget.y = pt.y;
 
